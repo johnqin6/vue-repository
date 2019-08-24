a14bf22c5f24b8aa31653a088e7d6e4fbd2c3b5c
秦振坤 <johnqin@rektec.com.cn>
2019-08-24 09:43:30 +0800

    公共样式完成

--- vue-toutiao/src/main.js
+++ vue-toutiao/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+/* 样式 */
+import './styles/index.css';
+import './styles/index.less';
+
 Vue.config.productionTip = false;
 
 new Vue({

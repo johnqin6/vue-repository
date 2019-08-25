f3c9c7288a8c3c5deefff56c418d0e44f91fb5d4
秦振坤 <johnqin@rektec.com.cn>
2019-08-25 17:23:27 +0800

    添加懒加载等插件和工具方法

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastclick from 'fastclick' // 防止移动端设备点击屏幕延迟(300毫秒)，为检测用户是否双击，使其立即响应用户点击事件
+import VueLazyLoad from 'vue-lazyload' // 懒加载
+
+import './assets/css/less/index.less'
+
+fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/image/default.png')
+})
 
 Vue.config.productionTip = false
 

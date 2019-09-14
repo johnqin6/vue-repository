e7fbadc5e9f930c32b5968c2109261ef8ca44b43
秦振坤 <johnqin@rektec.com.cn>
2019-09-14 10:54:32 +0800

    静态资源和样式及工具函数

--- vue-admin-webapp/src/main.js
+++ vue-admin-webapp/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/assets/iconfont/iconfont.css'
+import './plugins/element'
+import animated from 'animate.css'
+import './style/index.scss'
+
+Vue.use(animated)
 
 Vue.config.productionTip = false
 
